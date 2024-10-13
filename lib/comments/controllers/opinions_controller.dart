67fd4fc956bd2f99c1bdb385359e40228f62286a
EmeritusThinkPad <jairparedesal123@gmail.com>
2024-10-13 10:04:00 -0500

    errors and exceptions implemented

--- lib/comments/controllers/opinions_controller.dart
+++ lib/comments/controllers/opinions_controller.dart
@@ -1,9 +1,16 @@
 import 'package:get/get.dart';
 
+List<String> personalDataOptions = ['SI', 'NO'];
+
 class OpinionsController extends GetxController {
   var name = 'prueba'.obs;
+  var personalDataOptionSelected = personalDataOptions[0].obs;
 
-  changeWord(String word) {
+  void changeWord(String word) {
     name.value = word;
+  }
+
+  void changePersonalDataOption(String option) {
+    personalDataOptionSelected.value = option;
   }
 }
