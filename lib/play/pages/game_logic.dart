0dab9d57fd1ed2f63b6ee26d1a3f6d805aa15a75
William Perez <wulli.mu28@gmail.com>
2024-10-21 18:15:08 -0500

    fixed juego

--- lib/play/pages/game_logic.dart
+++ lib/play/pages/game_logic.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,41 +8,55 @@ class GameLogic {
   String level = '';
 
   late List<String> card_list = [];
-  late List<bool> isEnabled = [true, true, true, true, false, true,true, true, true /*, true, true, true,true, true, true, true, true, true, true*/];
+  late List<bool> isEnabled = [
+    true,
+    true,
+    true,
+    true,
+    false,
+    true,
+    true,
+    true,
+    true /*, true, true, true,true, true, true, true, true, true, true*/
+  ];
 
   var axiCount = 0;
   var cardCount = 0;
   List<Map<int, String>> matchCheck = [];
 
-
   void initGame(BuildContext context) {
-    
-      cardCount = 9; //16;
-      axiCount = 3; //4;
-      card_list = [
-        'lib/assets/img4.JPG',
-        'lib/assets/img1.JPG',
-        'lib/assets/img2.JPG',
-        'lib/assets/img3.JPG',
-
-        'lib/assets/logo2.png',
-        
-        'lib/assets/img4.JPG',
-        'lib/assets/img1.JPG',
-        'lib/assets/img2.JPG',
-        'lib/assets/img3.JPG',
-        /*'lib/assets/logo2.png',
+    cardCount = 9; //16;
+    axiCount = 3; //4;
+    card_list = [
+      'lib/assets/2dd50e12-9829-4460-ab85-12cd2df9f32e.JPG',
+      'lib/assets/5a0a96ad-cbf9-4902-b997-224a519e285d.JPG',
+      'lib/assets/a9dea6e8-7af5-4411-b49a-1c317124ba8f.JPG',
+      'lib/assets/b470655f-0bec-4670-939e-c203b6d2d4ef.JPG',
+      'lib/assets/logo2.png',
+      'lib/assets/2dd50e12-9829-4460-ab85-12cd2df9f32e.JPG',
+      'lib/assets/5a0a96ad-cbf9-4902-b997-224a519e285d.JPG',
+      'lib/assets/a9dea6e8-7af5-4411-b49a-1c317124ba8f.JPG',
+      'lib/assets/b470655f-0bec-4670-939e-c203b6d2d4ef.JPG',
+      /*'lib/assets/logo2.png',
         'lib/assets/logo2.png',
         'lib/assets/logo2.png',
         'lib/assets/logo3.png',
         'lib/assets/logo3.png',
         'lib/assets/logo3.png',
         'lib/assets/logo3.png',*/
-      ];
+    ];
 
-
-    isEnabled = [true, true, true, true, false, true,true, true, true /*, true, true, true,true, true, true, true, true, true, true*/];
-
+    isEnabled = [
+      true,
+      true,
+      true,
+      true,
+      false,
+      true,
+      true,
+      true,
+      true /*, true, true, true,true, true, true, true, true, true, true*/
+    ];
 
     //card_list.shuffle();
 
