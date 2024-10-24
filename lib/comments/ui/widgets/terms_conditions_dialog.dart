6c58476e70f46a45544bdfba2ea91ede0235c34b
EmeritusThinkPad <jairparedesal123@gmail.com>
2024-10-24 17:40:53 -0500

    corazones en 0

--- lib/comments/ui/widgets/terms_conditions_dialog.dart
+++ lib/comments/ui/widgets/terms_conditions_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TermsConditionsDialog extends StatefulWidget {
   final Function onAccept;
@@ -31,10 +32,32 @@ class _TermsConditionsDialogState extends State<TermsConditionsDialog> {
               size: 100,
             ),
             const SizedBox(height: 10),
-            Text(
-                'Para continuar, debes confirmar que tienes al menos 18 años y aceptar nuestros [Términos y Condiciones].',
-              style: Theme.of(context).textTheme.titleMedium,
+            Column(
+              children: [
+                Text(
+                  'Para continuar, debes confirmar que tienes al menos 18 años y aceptar nuestros:',
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                GestureDetector(
+                  onTap: () async {
+                    final Uri url = Uri.parse(
+                        'https://tusitio.com/terminos'); // Reemplaza con tu URL
+                    if (await canLaunchUrl(url)) {
+                      await launchUrl(url);
+                    } else {
+                      throw 'No se pudo abrir $url';
+                    }
+                  },
+                  child: Text(
+                    'Términos y Condiciones',
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: Colors.blue, // Color para parecer un enlace
+                      decoration: TextDecoration.underline, // Subrayado para parecer un enlace
                     ),
+                  ),
+                ),
+              ],
+            ),
             Padding(
               padding: const EdgeInsets.only(top: 15),
               child: Row(
@@ -63,26 +86,25 @@ class _TermsConditionsDialogState extends State<TermsConditionsDialog> {
                   SizedBox(width: 10),
                   Expanded(
                       child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context).primaryColor,
-                          padding: const EdgeInsets.symmetric(vertical: 15),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(12),
-                          ),
-                        ),
-                        onPressed: () async {
-                          Navigator.of(context).pop();
-                          widget.onAccept();
-                        },
-                        child: Text(
-                          'Confirmar',
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleSmall
-                              ?.copyWith(color: Colors.white, fontSize: 18),
-                        ),
-                      )),
-
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    onPressed: () async {
+                      Navigator.of(context).pop();
+                      widget.onAccept();
+                    },
+                    child: Text(
+                      'Confirmar',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall
+                          ?.copyWith(color: Colors.white, fontSize: 18),
+                    ),
+                  )),
                 ],
               ),
             ),
